d64870c033b21ce1a18eb28b08519dd4c4135fc6
YakunichkinAV <yakunichkinav@multibonus.ru>
2021-02-26 23:23:30 +0300

    fixed

--- src/test/java/tests/ToolsQAFormTest.java
+++ src/test/java/tests/ToolsQAFormTest.java
@@ -3,20 +3,27 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ToolsQAFormTest {
 
     @Test
     void toolsQAFormTest(){
+        String phone = "8989898989";
+        String firstName = "Alex";
+        String lastName = "Yak";
+        String gender = "Male";
+
         open("https://demoqa.com/automation-practice-form");
-        $("#firstName").setValue("Alex");
-        $("#lastName").setValue("Yak");
-        $x(".//*[@id='gender-radio-1']/..").click();
-        $("#userNumber").setValue("89898989898");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $(byText(gender)).click();
+        $("#userNumber").setValue(phone);
         $("#submit").scrollTo().click();
-        $x(".//*[text()='Student Name']/following-sibling::td").shouldBe(text("Alex"));
-        $x(".//*[text()='Gender']/following-sibling::td").shouldBe(text("Male"));
-        $x(".//*[text()='Mobile']/following-sibling::td").shouldBe(text("8989898989"));
+
+        $x(".//*[text()='Student Name']").sibling(0).shouldBe(text(firstName + " " + lastName));
+        $x(".//*[text()='Gender']/following-sibling::td").shouldBe(text(gender));
+        $x(".//*[text()='Mobile']/following-sibling::td").shouldBe(text(phone));
     }
 }
